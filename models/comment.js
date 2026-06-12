51f59e547bef10122552a8d5cb306b8d5d9dc52a
Kabir Hasan <hasankabir2002@gmail.com>
2026-06-12 22:22:42 +0600

    fixed mysql promes issu

--- models/comment.js
+++ models/comment.js
@@ -1,33 +1,35 @@
 const db = require("../database/db");
-module.exports.DeleteComment = (comment_id)=>{
-  return db.promise().query(
-    `delete from comment
-    where comment_id =?`,
+
+module.exports.DeleteComment = (comment_id) => {
+  return db.query(
+    `DELETE FROM comment
+     WHERE comment_id = ?`,
     [comment_id]
   );
 };
-module.exports.InsertVlaue = (post_id,user_id,comment)=>{
-  return db.promise().query(
-      `insert into comment(comment,post_id,user_id)
-       values
-       (?,?,?);
-      `,
-      [comment,post_id,user_id]
+
+module.exports.InsertVlaue = (post_id, user_id, comment) => {
+  return db.query(
+    `INSERT INTO comment(comment, post_id, user_id)
+     VALUES (?, ?, ?)`,
+    [comment, post_id, user_id]
   );
 };
-module.exports.findOnebyID = (comment_id)=>{
-  return db.promise().query(
-    `select * from comment
-    where comment_id=?`,
+
+module.exports.findOnebyID = (comment_id) => {
+  return db.query(
+    `SELECT *
+     FROM comment
+     WHERE comment_id = ?`,
     [comment_id]
   );
 };
-module.exports.editComment = (comment_id,comment)=>{
-  return db.promise().query(
-    `update comment
-    set comment=?
-    where comment_id=?`,[
-      comment,comment_id
-    ]
+
+module.exports.editComment = (comment_id, comment) => {
+  return db.query(
+    `UPDATE comment
+     SET comment = ?
+     WHERE comment_id = ?`,
+    [comment, comment_id]
   );
 };
