56b8c37a083aae082ab2d7ddf1c734c1d3cb7e9a
Kabir Hasan <hasankabir2002@gmail.com>
2026-06-17 12:36:18 +0600

    fix cloudinary storage

--- models/comment.js
+++ models/comment.js
@@ -1,13 +1,13 @@
 const db = require("../database/db");
 module.exports.DeleteComment = (comment_id)=>{
-  return db.promise().query(
+  return db.query(
     `delete from comment
     where comment_id =?`,
     [comment_id]
   );
 };
 module.exports.InsertVlaue = (post_id,user_id,comment)=>{
-  return db.promise().query(
+  return db.query(
       `insert into comment(comment,post_id,user_id)
        values
        (?,?,?);
@@ -16,14 +16,14 @@ module.exports.InsertVlaue = (post_id,user_id,comment)=>{
   );
 };
 module.exports.findOnebyID = (comment_id)=>{
-  return db.promise().query(
+  return db.query(
     `select * from comment
     where comment_id=?`,
     [comment_id]
   );
 };
 module.exports.editComment = (comment_id,comment)=>{
-  return db.promise().query(
+  return db.query(
     `update comment
     set comment=?
     where comment_id=?`,[
--- models/like.js
+++ models/like.js
@@ -1,28 +1,28 @@
 const db = require("../database/db");
 
 module.exports.findLike = (post_id,user_id)=>{
-  return db.promise().query(
+  return db.query(
     `SELECT * FROM likes WHERE post_id=? AND user_id=?`,
     [post_id,user_id]
   );
 };
 
 module.exports.addOne = (post_id,user_id)=>{
-  return db.promise().query(
+  return db.query(
     `INSERT INTO likes(post_id,user_id) VALUES(?,?)`,
     [post_id,user_id]
   );
 };
 
 module.exports.removeOne = (post_id,user_id)=>{
-  return db.promise().query(
+  return db.query(
     `DELETE FROM likes WHERE post_id=? AND user_id=?`,
     [post_id,user_id]
   );
 };
 
 module.exports.countLike = (post_id)=>{
-  return db.promise().query(
+  return db.query(
     `SELECT COUNT(*) AS total FROM likes WHERE post_id=?`,
     [post_id]
   );
